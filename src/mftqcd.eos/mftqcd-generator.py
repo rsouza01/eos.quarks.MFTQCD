cffca81027bf58bbd212a7278958bd85a391cf4e
Rodrigo Souza <rsouza01@gmail.com>
2015-07-01 23:15:35 -0300

    File renaming and other stuff.

--- src/mftqcd.eos/mftqcd-generator.py
+++ src/mftqcd.eos/mftqcd-generator.py
@@ -1,49 +1,42 @@
-#!/usr/bin/python
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
 
-#================================================
-# Authors:  
-#		Rodrigo Souza 
-#		<rsouza01@gmail.com>
-#       2015-06-07
-#================================================
+# mftqcd.eos - MFTQCD Equation of State generator
+# Copyright (C) 2002015  Rodrigo Alvares de Souza <rsouza01@gmail.com>
 
-#================================================
-#  Numerical and plotting libraries
-#================================================
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 
 from datetime import datetime
 
-"""
-Escolha um ou outro.
-"""
-import eos_generator as eos
+import mftqcd as eos
 
-import sys
-import getopt
-import numpy as np
-from enum import Enum
-
-#opts, args = getopt.getopt(sys.argv, "r", ["redir"])
-
-#for opt, arg in opts:
-#	if opt in ("-r", "--redir"):
-#		sys.stdout = open("./eos.csv", "w")
-#print opts , args
-
-
-#================================================
+# ================================================
 # Initialization Routines
-#================================================
+# ================================================
 initTime = datetime.now()
 
 
-#================================================
+# ================================================
 
 eos.fFuncMain()
 
-#================================================
+# ================================================
 # Finalization Routines
-#================================================
+# ================================================
 
 
 endTime = datetime.now()
@@ -51,5 +44,3 @@ print "\n\n\n================================================"
 print "Time elapsed: %d ms." % ((endTime - initTime).microseconds)
 print "Done!"
 print "================================================"
-
-
